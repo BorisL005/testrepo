444c3fb2f980aac2919cf6da9d682e71c621a4b2
Boris <blepeshenkov@yaypay.com>
2022-10-07 19:53:54 +0300

    [0002] - Added analogue counter

---
+++ src/com/company/AnlogueCounter.java
@@ -0,0 +1,45 @@
+/*
+ * *************************************************************************
+ * * Yaypay CONFIDENTIAL   2022
+ * * All Rights Reserved. * *
+ * NOTICE: All information contained herein is, and remains the property of Yaypay Incorporated and its suppliers, if any.
+ * The intellectual and technical concepts contained  herein are proprietary to Yaypay Incorporated
+ * and its suppliers and may be covered by U.S. and Foreign Patents, patents in process, and are protected by trade secret or copyright law.
+ * Dissemination of this information or reproduction of this material  is strictly forbidden unless prior written permission is obtained  from Yaypay Incorporated.
+ */
+
+package com.company;/*
+ * Author : Boris Lepeshenkov
+ * Date Created: 2022/10/07
+ */
+
+public class AnlogueCounter extends Counter {
+    public AnlogueCounter(int initialValue) {
+        super(initialValue);
+    }
+
+    @Override
+    public void addOne() {
+        super.addOne();
+    }
+
+    @Override
+    public void minusOne() {
+        super.minusOne();
+    }
+
+    @Override
+    public void printCurrentNumber(String msg) {
+        super.printCurrentNumber(msg);
+    }
+
+    @Override
+    public int getNumber() {
+        return super.getNumber();
+    }
+
+    @Override
+    public void setNumber(int number) {
+        super.setNumber(number);
+    }
+}
